d728b3c72c9db85d312b4e9cb9ef1eb6792838d0
Makar Kudryavtsev <makarkudryavtsev@mail.ru>
2025-12-25 20:57:04 +0300

    add(models): rename ToDo struct to Todo and implement GetAll and GetByID methods in ToDoStorage

--- internal/repository/todo.go
+++ internal/repository/todo.go
@@ -9,22 +9,26 @@ import (
 
 var (
 	errDuplicateID = errors.New("todo с данным ID уже существует")
+	errNotFound    = errors.New("todo не найден")
 )
 
 type (
+	// ToDoStorage потокобезопасное хранилище для объектов models.Todo.
 	ToDoStorage struct {
 		mu    sync.RWMutex
-		items map[int]models.ToDo
+		items map[int]models.Todo
 	}
 )
 
+// NewToDoStorage создает и возвращает новый экземпляр ToDoStorage.
 func NewToDoStorage() *ToDoStorage {
 	return &ToDoStorage{
-		items: make(map[int]models.ToDo),
+		items: make(map[int]models.Todo),
 	}
 }
 
-func (s *ToDoStorage) Create(todo models.ToDo) error {
+// Create добавляет новый объект в хранилище.
+func (s *ToDoStorage) Create(todo models.Todo) error {
 	s.mu.Lock()
 	defer s.mu.Unlock()
 
@@ -36,9 +40,31 @@ func (s *ToDoStorage) Create(todo models.ToDo) error {
 	return nil
 }
 
-// TODO: func (s *ToDoStorage) GetAll() []models.ToDo {}
+// GetAll возвращает все объекты из хранилища.
+func (s *ToDoStorage) GetAll() []models.Todo {
+	s.mu.RLock()
+	defer s.mu.RUnlock()
 
-// TODO: func (s *ToDoStorage) GetByID(id int) (models.ToDo, error) {}
+	result := make([]models.Todo, 0, len(s.items))
+	for _, todo := range s.items {
+		result = append(result, todo)
+	}
+
+	return result
+}
+
+// GetByID возвращает объект по его ID.
+func (s *ToDoStorage) GetByID(id int) (models.Todo, error) {
+	s.mu.RLock()
+	defer s.mu.RUnlock()
+
+	todo, exists := s.items[id]
+	if !exists {
+		return models.Todo{}, errNotFound
+	}
+
+	return todo, nil
+}
 
 // TODO: func (s *TodoStorage) Update(todo models.ToDo) error {}
 
