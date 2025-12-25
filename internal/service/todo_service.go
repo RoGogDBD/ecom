267908346167bf8a5541d1cdff5a6e5b6c079524
Makar Kudryavtsev <makarkudryavtsev@mail.ru>
2025-12-25 21:50:48 +0300

    add(service): create TodoService with storage interface

--- internal/service/todo_service.go
+++ internal/service/todo_service.go
@@ -10,3 +10,10 @@ type (
 func NewTodoService(storage Storage) *TodoService {
 	return &TodoService{storage: storage}
 }
+
+// TODO: func (s *TodoService) Create(todo models.Todo) error {}.
+// TODO: func (s *TodoService) Update(todo models.Todo) error {}.
+// TODO: func (s *TodoService) Delete(id int) error {}.
+// TODO: func (s *TodoService) GetAll() []models.Todo {}.
+// TODO: func (s *TodoService) GetByID(id int) (models.Todo, error) {}.
+// TODO: func validateTodo(todo models.Todo) error {}.
